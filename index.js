751d63e2e0602291859245cd54785228060763c5
nfluk <nickylukic@gmail.com>
2022-05-10 07:44:35 +0200

    refactoring

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 // Given a random non-negative number,
 // you have to return the digits of this number within an array in reverse order.
 
-function digitize(n) {
-  const myArray = [];
-  let digits = n
-    .toString()
-    .split('')
-    .map((num) => parseInt(num));
+// function digitize(n) {
+//   const myArray = [];
+//   let digits = n
+//     .toString()
+//     .split('')
+//     .map((num) => parseInt(num));
 
-  for (let i = 0; i < digits.length; i++) {
-    myArray.unshift(digits[i]);
-  }
+//   for (let i = 0; i < digits.length; i++) {
+//     myArray.unshift(digits[i]);
+//   }
 
-  return myArray;
+//   return myArray;
+// }
+
+// refactored:
+
+function reversing(n) {
+  return String(n).split('').map(Number).reverse();
 }
